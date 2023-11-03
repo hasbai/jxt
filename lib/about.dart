48f125489b09d993c9e0768cc0ff43237f3b37de
hasbai <34417826+hasbai@users.noreply.github.com>
2023-11-04 01:34:30 +0800

    feat: about

--- lib/about.dart
+++ lib/about.dart
@@ -1,12 +1,21 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:jxt_toolkits/components/layout.dart';
 
 class About extends StatelessWidget {
   const About({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('About'),
+    return Skeleton(
+        appbar: AppBar(title: const Text('About')),
+        body: const Center(child:
+          Expanded(child:
+            Text("This is JXT's toolkits.\n"
+                "One tool is for booking the laundry room.\n"
+                "More features are currently under development..."
+              ),
+          )
+        )
     );
   }
 }
